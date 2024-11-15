61b49a4f3fe0a9929f6643dc2e5d70a234a83a98
olafisKem <folafis@googlemail.com>
2024-11-15 11:01:16 +0000

    chore: test setup

---
+++ closest-to-zero.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "@jest/globals"
+
+describe('closestToZero', () => {
+    it('should fail', () => {
+        expect(true).toBe(true);
+    });
+});
---
+++ closest-to-zero.ts
@@ -0,0 +1,3 @@
+// Receive a list of integers.
+// Return the integer closest to zero.
+// If there is a tie (e.g., -3 and 3), return the positive value.
