b4003b05f448817026922129d7dedd27101592e6
olafisKem <folafis@googlemail.com>
2024-11-15 11:25:48 +0000

    chore: single number passing

--- closest-to-zero.ts
+++ closest-to-zero.ts
@@ -1,4 +1,20 @@
 
+// ({}–>nil) no code at all->code that employs nil
+// (nil->constant)
+// (constant->constant+) a simple constant to a more complex constant
+// (constant->scalar) replacing a constant with a variable or an argument
+// (statement->statements) adding more unconditional statements.
+// (unconditional->if) splitting the execution path
+// (scalar->array)
+// (array->container)
+// (statement->recursion)
+// (if->while)
+// (expression->function) replacing an expression with a function or algorithm
+// (variable->assignment) replacing the value of a variable.
 export const closestToZero = (xs: number[]) => {
-    return null;
+    if (xs.length === 0) {
+      return null;
+    }
+
+    return 1;
 }
